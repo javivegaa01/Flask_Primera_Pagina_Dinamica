7dba652bd5a67e5541ee16ad8223be32c36130ac
Javier Vega Correa <javier@debian>
2021-04-19 17:34:49 +0200

    potencias terminadas

--- app.py
+++ app.py
@@ -14,4 +14,16 @@ def pagina1():
 def pagina2():
     return render_template("pagina2.html")
 
+@app.route('/potencia/<int:base>/<exponente>',methods=["GET","POST"])
+def potencia(base,exponente):
+    exponente=int(exponente)
+    if exponente>0:
+        res = base**exponente
+    elif exponente==0:
+        res = 1
+    elif exponente<0:
+        res = base**exponente
+    else:
+        abort(404)
+    return render_template("potencia.html",base=base,exponente=exponente,resultado=res)
 app.run("0.0.0.0",8000,debug=True)
