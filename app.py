2c5415a4c8c998229f77fa6dd2084f6c547571f5
Javier Vega Correa <javier@debian>
2021-04-19 23:06:02 +0200

    cintar cadenas

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def pagina2():
     return render_template("pagina2.html")
 
 @app.route('/potencia/<int:base>/<exponente>',methods=["GET","POST"])
-def potencia(base,exponente):
+def calcular_potencia(base,exponente):
     exponente=int(exponente)
     if exponente>0:
         res = base**exponente
@@ -26,4 +26,13 @@ def potencia(base,exponente):
     else:
         abort(404)
     return render_template("potencia.html",base=base,exponente=exponente,resultado=res)
+
+@app.route('/cuenta/<palabra>/<letras>',methods=["GET","POST"])
+def cuenta_letra(palabra,letras):
+    if len(letras)!=1:
+        abort(404)
+    else:
+        num_veces=palabra.count(letras)
+    return render_template("palabras.html",palabra=palabra,letras=letras,num_veces=num_veces)
+
 app.run("0.0.0.0",8000,debug=True)
